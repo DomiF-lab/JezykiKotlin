b15e7b2d907a045304a645fab10b2f413c321cf0
DomiF-lab <253449@student.pwr.edu.pl>
2025-03-23 22:14:22 +0100

    Rozwiazanie zadania 1

--- app/src/main/java/com/example/jezykikotlin/Lista1Zad1.kt
+++ app/src/main/java/com/example/jezykikotlin/Lista1Zad1.kt
@@ -1,5 +1,23 @@
-package com.example.jezykikotlin
+package com.example.jezykikotlin.zad1
+import kotlin.math.sqrt
+
+
+fun heron(a:Double, b:Double, c:Double): Double {
+    //czy boki sa dodatnie
+    if (a <= 0 || b <= 0 || c <= 0) {
+        throw IllegalArgumentException("Boki nie moga byc mniejsze lub rowne 0!")
+    }
+
+    //warunek nierownosci
+    if (a + b <= c || a + c <= b || b + c <= a ) {
+        throw IllegalArgumentException("Nie zostal spelniony warunek nierownosci")
+    }
+
+    val p = 0.5 * (a+b+c)
+    return sqrt( p* (p-a) * (p-b) * (p-c) )
+}
 
 fun main() {
-    println("GIT test")
+    val trojkat = heron(2.0,5.0,6.0)
+    println(trojkat)
 }
